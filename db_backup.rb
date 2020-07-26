f895a454f3ce18beaa5c64466408614be5f8bbb9
Gustavo Guichard <gustavoguichard@gmail.com>
2020-07-26 12:17:13 -0300

    Add --no-gzip option

--- db_backup.rb
+++ db_backup.rb
@@ -7,15 +7,21 @@ require 'open3'
 
 def run_command(command, statuscode = 1)
   puts "Running '#{command}'"
+
+  yield if block_given?
+
   _stdout_str, stderr_str, status = Open3.capture3(command)
   return if status.exitstatus.zero?
 
-  $stderr.puts "There was a problem running '#{command}'"
-  $stderr.puts stderr_str
+  warn "There was a problem running '#{command}'"
+  warn stderr_str
   exit statuscode
 end
 
-options = {}
+options = {
+  gzip: true
+}
+
 option_parser = OptionParser.new do |opts|
   executable_name = File.basename($PROGRAM_NAME)
   opts.banner = "Backup one or more MySQL databases
@@ -24,16 +30,20 @@ Usage: #{executable_name} [options] database_name
 
 "
 
-  opts.on('-i', '--iteration', 'Indicate that this backup is an "iteration" backup') do
-    options[:iteration] = true
-  end
-
-  opts.on('-u USER', '--user', /^.+\..+$/, 'Database username, in first.last format') do |user|
+  opts.on('-u USER', '--username', /^.+\..+$/, 'Database username, in first.last format') do |user|
     options[:user] = user
   end
 
   opts.on('-p PASSWORD', '--password', 'Database password') do |password|
     options[:password] = password
+  end
+
+  opts.on('-i', '--end-of-iteration', 'Indicate that this backup is an "iteration" backup') do
+    options[:iteration] = true
+  end
+
+  opts.on('--no-gzip', 'Do not compress the backup file') do
+    options[:gzip] = false
   end
 end
 
@@ -57,5 +67,5 @@ Signal.trap('SIGINT') do
   exit 1
 end
 
-run_command "mysqldump #{auth}#{database} > #{backup_file}"
-run_command "gzip #{backup_file}", 2
+run_command("mysqldump #{auth}#{database} > #{backup_file}")
+run_command("gzip #{backup_file}", 2) if options[:gzip]
